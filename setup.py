1168d3255e0d67fbf74a9c71feaccbdafef59d21
Corey Farwell <coreyf@rwell.org>
2016-02-09 14:14:55 -0500

    Bump version to 0.6.10

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='mock-django',
-    version='0.6.9',
+    version='0.6.10',
     description='',
     license='Apache License 2.0',
     author='David Cramer',
